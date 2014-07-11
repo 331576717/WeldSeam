89ba78296194af4513e14ad0669a32c44bcaec1f
331576717 <331576717@qq.com>
2014-07-11 18:51:31 +0800

    完成焊接测试

--- C++/Locate/Test/findContourEdge.cpp
+++ C++/Locate/Test/findContourEdge.cpp
@@ -26,6 +26,8 @@ HANDLE g_hProcSemaphore;
 HANDLE g_hCom;
 OVERLAPPED g_wrOverlapped;
 
+CONST int XLOCATIONBOUND = 160001;
+CONST int ZLOCATIONBOUND = 57000;
 vector<Point3i> g_vP;
 vector<Speed> g_vS;
 int g_pointCount = 0;
@@ -39,23 +41,28 @@ DWORD WINAPI TimingThread(LPVOID param)
 		cout << "Timing" << endl;
 		ReleaseSemaphore(g_hProcSemaphore, 1, NULL);
 		
-		Sleep(10000);
+		Sleep(2000);
 		
 	}
 
 }
+
 DWORD WINAPI ImgProcThread(LPVOID param)
 {
 	while (true)
 	{
 		WaitForSingleObject(g_hProcSemaphore, INFINITE);
-		FormateData(g_vP[g_pointCount], g_vS[g_pointCount], g_buffer);
-		g_pointCount++;
-		cout << "Processing" << "Y: " << g_vP[g_pointCount].y << endl;
-		ReleaseSemaphore(g_hTimingSemaphore, 1, NULL);
+		if (g_pointCount < 14)
+		{
+			FormateData(g_vP[g_pointCount], g_vS[g_pointCount], g_buffer);
+			g_pointCount++;
+			cout << "Processing" << "Y: " << g_vP[g_pointCount].y << endl;
+			ReleaseSemaphore(g_hTimingSemaphore, 1, NULL);
+		}
 		//Sleep(500);
 	}
 }
+
 void StartWeld()
 {
 	FormateData(Point3i(8000, 4000, 0), Speed(3000, 3000, 3000), g_buffer);
@@ -71,38 +78,74 @@ void StartWeld()
 	g_pointCount++;
 	Sleep(2000);
 }
+
 void StartWeldTest()
 {
-	FormateData(Point3i(8000, 400, 0), Speed(3000, 3000, 3000), g_buffer);
+	FormateData(Point3i(15000, 5000, 0), Speed(20000, 20000, 3000), g_buffer);
 	SendData(g_hCom, g_wrOverlapped, g_buffer);
 	Sleep(10000);
-	FormateData(Point3i(8000, 400, 30000), Speed(3000, 3000, 40000), g_buffer);
+	FormateData(Point3i(15000, 5000, 17500), Speed(3000, 3000, 30000), g_buffer);
 	SendData(g_hCom, g_wrOverlapped, g_buffer);
  	Sleep(1100);
-	FormateData(Point3i(8000, 400, 28000), Speed(3000, 3000, 20000), g_buffer);
+	FormateData(Point3i(15000, 5000, 16000), Speed(3000, 3000, 30000), g_buffer);
 	SendData(g_hCom, g_wrOverlapped, g_buffer);
 	Sleep(1100);
-	FormateData(g_vP[g_pointCount], g_vS[g_pointCount], g_buffer);
-	g_pointCount++;
-	Sleep(1100);
+	FormateData(Point3i(150000, 2000, 61000), Speed(3300, 3300, 1000), g_buffer);
+	SendData(g_hCom, g_wrOverlapped, g_buffer);
+	//FormateData(g_vP[g_pointCount], Speed(2000,2000,1000), g_buffer);
+	//g_pointCount++;
+	VideoCapture cap(0); // open the default camera
+	cap.set(CV_CAP_PROP_FRAME_WIDTH,1024.0);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT,768.0);
+	Mat frame;
+	for (int i = 0; i < 610; i++)
+	{
+		cap >> frame; // get a new frame from camera
+		//frame = imread("E:\\º¸½Ó¶¨Î»\\pictures\\20140527\\18_11_46.jpg");
+		//Rect rec(300, 100, 300, 650);
+		//int p = TestHoughLines(frame(rec));
+		//circle(frame, Point(p.x, p.y), 4, Scalar(255, 255, 255), 2);
+		imshow("video", frame);
+		waitKey(50);
+	}
+	//Sleep(40000);
+	FormateData(Point3i(150000, 2000, 0), Speed(3300, 3300, 20000), g_buffer);
+	SendData(g_hCom, g_wrOverlapped, g_buffer);
 }
 
 int main()
 {
+	bool flag = false;
+	bool ini = InitCom(g_hCom, g_wrOverlapped);
+
+	FormateData(Point3i(10, 10, 10), Speed(3300, 3300, 0), g_buffer);
+	flag = SendData(g_hCom, g_wrOverlapped, g_buffer);
+	cout << flag << endl;
+
+	FormateData(Point3i(20000, 0, 0), Speed(3300, 3300, 0), g_buffer);
+	SendData(g_hCom, g_wrOverlapped, g_buffer);
+	cout << flag << endl;
+	Sleep(1000);
+
+	FormateData(Point3i(40000, 2000, 0), Speed(3300, 3300, 0), g_buffer);
+	SendData(g_hCom, g_wrOverlapped, g_buffer);
+	cout << flag << endl;
+
 	ifstream fin;
 	fin.open("C:\\Users\\James\\Desktop\\res2.txt");
 	for (size_t i = 0; i < 15; i++)
 	{
-		int x, y, z, xSpped, ySpeed, zSpeed;
+		unsigned int x, y, z, xSpeed, ySpeed, zSpeed;
 		fin >> x >> y >> z;
-		x = 8000 + 10000*i;
-		y = 400;
-		z = 29000 + (i+1) * 10000;
+		x = (10000 + 10000*i) % XLOCATIONBOUND;
+		y = 2000;
+		z = 77000;
 		g_vP.push_back(Point3i(x, y, z));
 
-		fin >> xSpped >> ySpeed >> zSpeed;
-		zSpeed = 1000;
-		g_vS.push_back(Speed(xSpped, ySpeed, zSpeed));
+		fin >> xSpeed >> ySpeed >> zSpeed;
+		xSpeed = 3300;
+		zSpeed = 0;
+		g_vS.push_back(Speed(xSpeed, ySpeed, zSpeed));
 	}
 	//VideoCapture cap(0); // open the default camera
 	//cap.set(CV_CAP_PROP_FRAME_WIDTH,1024.0);
@@ -118,8 +161,8 @@ int main()
 	//	imshow("video", frame);
 	//	waitKey(50);
 	//}
-	//cap >> frame; // get a new frame from camera
-	bool ini = InitCom(g_hCom, g_wrOverlapped);
+
+	//bool ini = InitCom(g_hCom, g_wrOverlapped);
 
 	g_hTimingSemaphore = CreateSemaphore(NULL, 0, 1, NULL);
 	g_hProcSemaphore = CreateSemaphore(NULL, 0, 1, NULL);
@@ -128,7 +171,7 @@ int main()
 	CreateThread(NULL, 0, ImgProcThread, NULL, NULL, NULL);
 
 	StartWeldTest();
-
+	
 	/*FormateData(Point3i(10000, 20000, 3000), Speed(1000, 1000, 1000), buffer);
 	SendData(m_hCom, wrOverlapped, buffer);
 	int X = vP[0].x, Y = vP[0].y, Z = 14999;
